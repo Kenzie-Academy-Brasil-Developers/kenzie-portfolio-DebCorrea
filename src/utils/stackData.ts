82b193f523d7fba27ec0582ea97ba12261a6e586
Debora Correa <deboragomescorr@gmail.com>
2022-11-17 11:52:49 -0300

    feat: stacks adjusted

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,32 +1,77 @@
-import imgHTML from "../public/static/img/stack/html.svg";
-import imgCSS from "../public/static/img/stack/css.svg";
-import imgJS from "../public/static/img/stack/js.svg";
-// import imgNode from "../public/static/img/stack/node.svg";
-import imgStyled from "../public/static/img/stack/styled.svg";
-import imgReact from "../public/static/img/stack/react.svg";
-// import { FaVuejs } from "react-icons/fa";
-// import { SiTypescript } from "react-icons/si";
-// import { FaDocker } from "react-icons/fa";
+import { TiHtml5 } from "react-icons/ti";
+import { IoLogoCss3 } from "react-icons/io";
+import { SiJavascript } from "react-icons/si";
+import { DiNodejs } from "react-icons/di";
+import { SiStyledcomponents } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { DiDjango } from "react-icons/di";
+import { SiPostgresql } from "react-icons/si";
+import { BiGitMerge } from "react-icons/bi";
+import { AiFillGithub } from "react-icons/ai";
+import { SiRedux } from "react-icons/si";
 
 export const stackData = [
   {
     title: "HTML",
-    img: imgHTML,
+    img: TiHtml5,
   },
   {
     title: "CSS",
-    img: imgCSS,
+    img: IoLogoCss3,
   },
   {
     title: "JS",
-    img: imgJS,
+    img: SiJavascript,
   },
   {
     title: "Styled Components",
-    img: imgStyled,
+    img: SiStyledcomponents,
   },
   {
     title: "React",
-    img: imgReact,
+    img: FaReact,
+  },
+  {
+    title: "NodeJs",
+    img: DiNodejs,
+  },
+  {
+    title: "Python",
+    img: FaPython,
+  },
+  {
+    title: "Django",
+    img: DiDjango,
+  },
+  {
+    title: "TypeScript",
+    img: SiTypescript,
+  },
+  {
+    title: "PostgreSQL",
+    img: SiPostgresql,
+  },
+  {
+    title: "Docker",
+    img: FaDocker,
+  },
+  {
+    title: "Github",
+    img: AiFillGithub,
+  },
+  {
+    title: "Git",
+    img: BiGitMerge,
+  },
+  {
+    title: "Redux",
+    img: SiRedux,
+  },
+  {
+    title: "Context",
+    img: FaReact,
   },
 ];
